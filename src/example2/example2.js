e23aefb97885501e8ec45286317240f4da72e12e
VeereshaS431 <tridizi.dev1@gmail.com>
2024-09-07 00:57:37 +0530

    landing page

--- src/example2/example2.js
+++ src/example2/example2.js
@@ -1,16 +1,17 @@
 import React, { useState } from "react";
 import "./example2.css";
-import bpms1 from "../Assets/BPMS.jpg";
-import bpms2 from "../Assets/EMS.jpg";
-import bpms3 from "../Assets/HRMS.jpg";
-import bpms4 from "../Assets/LMS.jpg";
-import bpms5 from "../Assets/PMS.jpg";
+import bpms from "../Assets/BPMS.jpg";
+import ems from "../Assets/EMS.jpg";
+import hrms from "../Assets/HRMS.jpg";
+import lms from "../Assets/LMS.jpg";
+import pms from "../Assets/PMS.jpg";
 import logo from "../Assets/logo.png";
+import arrow from "../Assets/Arrow44.png"
 
 function App2() {
   const [hovered, setHovered] = useState(null);
 
-  const renderCard = (className, image,text,index) => (
+  const renderCard = (className, image, text, index) => (
     <div
       className={`card ${className} ${hovered === className ? "shrink" : ""}`}
       onMouseEnter={() => setHovered(className)}
@@ -20,26 +21,32 @@ function App2() {
         <h2 className={`text ${hovered === className ? `text${index}` : ""}`}>{text}</h2>
       </div>
       <div className="mask"></div>
-      <img style={{opacity:"0.4"}} src={image} alt="Background" className="background-image" />
+      <img style={{ opacity: "0.4" }} src={image} alt="Background" className="background-image" />
+      <div className="border_box">
+        <img src={arrow} alt="arrow" />
+      </div>
     </div>
   );
 
-//   const style1={
-//     background: "linear-gradient(206.88deg, #210379 12.71%, #FF5900 90.63%)",
+  //   const style1={
+  //     background: "linear-gradient(206.88deg, #210379 12.71%, #FF5900 90.63%)",
 
-//   }
+  //   }
 
   return (
     <>
       <div className="logo_container">
         <img src={logo} alt="logo" />
+        <div className="contact_box">
+         <p><span>Contact Us:</span> 7989389892</p>
+        </div>
       </div>
       <div className="container">
-        {renderCard("card1", bpms1,"Business Process Management System ",1)}
-        {renderCard("card2", bpms2,"Human Resource  Management System ",2)}
-        {renderCard("card3", bpms3,"Property Management System ",3)}
-        {renderCard("card4", bpms4,"E-Commerce Management System ",4)}
-        {renderCard("card5", bpms5,"Learning Management System ",5)}
+        {renderCard("card1", bpms, "Business Process Management System ", 1)}
+        {renderCard("card2", hrms, "Human Resource  Management System ", 2)}
+        {renderCard("card3", pms, "Property Management System ", 3)}
+        {renderCard("card4", ems, "E-Commerce Management System ", 4)}
+        {renderCard("card5", lms, "Learning Management System ", 5)}
       </div>
     </>
   );
