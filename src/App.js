b310758a0d8c1160f360090804303ebfb881501a
VeereshaS431 <tridizi.dev1@gmail.com>
2024-09-07 16:06:22 +0530

    landing-page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
 import './App.css';
 import App2 from './example2/example2';
-// import App1 from './example/example';
-// import LandingPage from './LandingPage/LandingPage';
-
 
 function App() {
   return (
     <div>
-      {/* <LandingPage/> */}
-      {/* <App1/> */}
       <App2/>
     </div>
   );
